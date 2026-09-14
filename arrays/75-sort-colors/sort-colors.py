0428020a752573164a9eaf028805984c7329b3a3
Lavanya Gupta <lavanyagupta136@gmail.com>
2026-09-14 09:48:00 +0530

    Time: 7 ms (7.59%) | Memory: 12.3 MB (91.37%) - LeetSync

---
+++ arrays/75-sort-colors/sort-colors.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def sortColors(self, nums):
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1,n):
+                if nums[i]>nums[j]:
+                    nums[i], nums[j] = nums[j], nums[i]
+        return nums
+
+
+              
